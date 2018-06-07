755cf95a5f4303a3904774f90218478b40222e2a
wojga476 <wojga476@gmail.com>
2018-06-07 18:03:01 +0200

    stworzenie tablicy liczb całkowitych

---
+++ src/TabLiczCal.java
@@ -0,0 +1,15 @@
+public class TabLiczCal {
+
+    public static void main(String[] args) {
+
+        int[] tablica = new int[5];
+        tablica[0]= 3;
+        tablica[1]= 5;
+        tablica[2]= 2;
+        tablica[3]= 7;
+        tablica[4]= 3;
+
+        System.out.println(tablica[0]+tablica[2]+tablica[4]);
+
+    }
+}
